2dde3ddce73e4224bacb504af66423ab91b1a412
Revanbtw <98419689+Revanbtw@users.noreply.github.com>
2022-03-30 09:54:28 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let canvas = document.getElementById("deez");
+let context = canvas.getContext("2d");
+context.fillStyle = "red";
+context.fillRect(0,0,1000,1000);
+
+let canvas2 = document.getElementById("deezis");
+let context2 = canvas2.getContext("2d");
+context2.fillStyle = "blue";
+context2.fillRect(0,0,1000,1000);
+class BBM {
+	constructor (x,y){
+		this.y = y
+		this.x = x
+	}
+
+
+
+
+
+
+
+
+}
